c91a0469774ce2ec212b0f309bf2821483164284
Pluto <96972840+plutosapiens@users.noreply.github.com>
2023-10-10 20:58:21 +0300

    CUB-3 Fix typo

--- src/config/expressConfig.js
+++ src/config/expressConfig.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const path = require("path");
 
-const expressConfig = (app) =>{
-    //Setup static files
-app.use(express.static(path.resolve(__dirname, "../public")));
-app.use(express.urlencoded({ extended: false }));
+const expressConfig = (app) => {
+    //Setup ststic files
+    app.use(express.static(path.resolve(__dirname, "../public")));
+    app.use(express.urlencoded({ extended: false }));
 };
 
 module.exports = expressConfig;
