d6714b957cca9caa7128c2d6b90aa0ef9fef514a
Pluto <96972840+plutosapiens@users.noreply.github.com>
2023-10-14 15:47:10 +0300

    CUB-3 Update cube shcema, adding user reference

--- src/models/Cube.js
+++ src/models/Cube.js
@@ -5,13 +5,16 @@ const cubeSchema = new mongoose.Schema({
     description: String,
     imageUrl: String,
     difficultyLevel: Number,
-
     accessories: [
         {
             type: mongoose.Types.ObjectId,//type from mongo
             ref: "Accessory" //the name of the model
         }
     ],
+    owner: {
+        type: mongoose.Types.ObjectId,
+        ref: 'User',
+    },
 });
 
 const Cube = mongoose.model('Cube', cubeSchema);
