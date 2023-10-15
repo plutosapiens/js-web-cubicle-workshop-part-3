0dc70896b8ed62d4f42668390c90412b0899dee0
Pluto <96972840+plutosapiens@users.noreply.github.com>
2023-10-15 13:12:34 +0300

    CUB-3 Implement route authentication guard

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -25,3 +25,11 @@ exports.auth = async (req, res, next) => {
     }
 
 };
+
+exports.isAuth = (req, res, next) => {
+    if(!req.user){
+        return res.redirect('/users/login');
+    }
+    
+    next();
+};
